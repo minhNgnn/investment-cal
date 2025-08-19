fa3f97d8e5bd8ffc7d7d4821e758418df23bd6f2
Minh Nguyen <tuanminh10082002gv@gmail.com>
2025-08-19 20:31:02 +0200

    header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <img
+        src="/investment-calculator-logo.png"
+        alt="Investment Calculator Logo"
+        id="header"
+      />
+      <h1>React Investment Calculator</h1>
+    </header>
+  );
+}
